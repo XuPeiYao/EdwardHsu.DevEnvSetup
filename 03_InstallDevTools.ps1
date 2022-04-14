f85da637e0c5e8e1c71fa6246c0486d6a898e448
Edward Hsu <st89382000@gmail.com>
2022-04-15 01:40:28 +0800

    init

---
+++ 03_InstallDevTools.ps1
@@ -0,0 +1,19 @@
+choco install openssl -y
+choco install git -y
+choco install dotnet-sdk -y
+choco install python3 -y
+choco install nodejs-lts -y
+choco install notepadplusplus -y
+choco install vscode -y
+choco install visualstudio2022community -y --package-parameters "--locale Zh-tw"
+choco install resharper --pre -y
+choco install dbeaver -y
+choco install putty -y
+choco install mremoteng -y
+choco install postman -y
+choco install microsoft-windows-terminal -y
+choco install stylecop -y
+choco install 7zip.install -y
+choco install microsoft-teams.install -y
+choco install cascadiacode -y
+choco install powertoys -y
