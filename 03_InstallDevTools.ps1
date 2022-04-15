f57edaacf709b4fb2df052950173f8f0085d8acf
EdwardHsu <st89382000@gmail.com>
2022-04-15 22:18:07 +0800

    feat: Add git-fork and full install vs2022

--- 03_InstallDevTools.ps1
+++ 03_InstallDevTools.ps1
@@ -5,7 +5,7 @@ choco install python3 -y
 choco install nodejs-lts -y
 choco install notepadplusplus -y
 choco install vscode -y
-choco install visualstudio2022community -y --package-parameters "--locale Zh-tw"
+choco install visualstudio2022community -y --package-parameters "--locale Zh-tw --allWorkloads --includeRecommended"
 choco install resharper --pre -y
 choco install dbeaver -y
 choco install putty -y
@@ -20,3 +20,8 @@ choco install powertoys -y
 choco install kubernetes-cli -y
 choco install lens -y
 choco install docker-desktop -y
+choco install firefox -y
+choco install googlechrome -y
+
+Invoke-WebRequest -Uri https://git-fork.com/update/win/ForkInstaller.exe -OutFile ForkInstaller.exe
+Start-Process -Filepath ForkInstaller.exe -ArgumentList "-s"
