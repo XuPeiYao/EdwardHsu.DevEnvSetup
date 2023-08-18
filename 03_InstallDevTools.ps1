2566fc1eb0a5add974085bafa92ddd485731a416
Edward Hsu <st89382000@gmail.com>
2023-08-18 23:38:37 +0800

    Update 03_InstallDevTools.ps1

--- 03_InstallDevTools.ps1
+++ 03_InstallDevTools.ps1
@@ -17,8 +17,6 @@ choco install microsoft-teams.install -y
 choco install firefox -y
 choco install googlechrome -y
 choco install vscode -y
-choco install visualstudio2022community -y --package-parameters "--locale Zh-tw --allWorkloads --includeRecommended"
-choco install resharper -y
 choco install stylecop -y
 choco install kubernetes-cli -y
 choco install kubernetes-helm -y
@@ -27,6 +25,9 @@ choco install docker-desktop -y
 choco install fiddler -y
 choco install winscp -y
 choco install make -y
+choco install goland -y
+choco install visualstudio2022community -y --package-parameters "--locale Zh-tw --allWorkloads --includeRecommended"
+choco install resharper -y
 
 Invoke-WebRequest -Uri https://git-fork.com/update/win/ForkInstaller.exe -OutFile ForkInstaller.exe
 Start-Process -Filepath ForkInstaller.exe -ArgumentList "-s"
